21c083f2dadd6142b49faac54e0be53f2b8bb612
Hani <139141380+Darkksoul@users.noreply.github.com>
2024-07-12 21:38:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,8 +43,7 @@ from __init__ import (
     replyDB,
 )
 from config import Config
-from helpers.database import database
-from helpers.forcesub import ForceSub
+from helpers import database
 from helpers.utils import UserSettings, get_readable_file_size, get_readable_time
 
 botStartTime = time.time()
